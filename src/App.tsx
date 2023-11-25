6911f66fd50f3abb5772de931c35e143ff3e3aa5
CElizOwens <owens.christina@gmail.com>
2023-11-24 23:26:06 -0800

    - Installed specific eslint plugins and modified eslint config as per Vite's recommendations for code meant for production - Added a basic prettier config file - Created a components folder - Created LandingPage component, moved all code returned by App.tsx into LandingPage.tsx, including Heirloom logo - Imported LandingPage.tsx into App.tsx and returned LandingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
-import { useState } from 'react'
-import heirloomLogo from '/Heirloomsmall.webp'
-import './App.css'
+import LandingPage from './components/LandingPage';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://www.heirloomrecordlabel.com/" target="_blank">
-          <img src={heirloomLogo} className="logo" alt="Heirloom logo" />
-        </a>
-      </div>
-      <h1>Palm & Vine Recording</h1>
-      <p className="subtitle">
-        Regional and traditional music records worth passing down
-      </p>
-      <div className="card">
-        <button className="landing-btn" onClick={() => setCount((count) => count + 1)}>
-          Enter site ({count})
-        </button>
-      </div>
-      <footer>
-        © 2023 Palm & Vine Recording | Website created by Natural Strings
-      </footer>
-    </>
-  )
+  return <LandingPage />;
 }
 
-export default App
+export default App;
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import heirloomLogo from '/Heirloomsmall.webp';
+
+export default function LandingPage() {
+  const [count, setCount] = useState(1);
+
+  return (
+    <>
+      <div>
+        <a
+          href="https://www.heirloomrecordlabel.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={heirloomLogo} className="logo" alt="Heirloom logo" />
+        </a>
+      </div>
+      <h1>Palm & Vine Recording</h1>
+      <p className="subtitle">
+        Regional and traditional music records worth passing down
+      </p>
+      <div className="card">
+        <button
+          className="landing-btn"
+          onClick={() => setCount((count) => count * 2)}
+        >
+          Enter site ({count})
+        </button>
+      </div>
+      <footer>
+        © 2023 Palm & Vine Recording | Website created by Natural Strings
+      </footer>
+    </>
+  );
+}

9444940be2a39e153a0dd79f7cfa96292d18985f
Nansamba Ssensalo <nssensalo@gmail.com>
2023-12-03 22:24:57 -0800

    nav bar components, added login and posts

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,10 @@
+import Posts from './Posts'
+import Artists from "./Artists"
+import Donations from "./Donations"
+import Login from "./Login"
+import Contact from "./Contact"
+
+
 export default function NavBar() {
   return (
     <nav className="navbar">

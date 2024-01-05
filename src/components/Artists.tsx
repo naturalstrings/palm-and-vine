974dfef4795252da70fcd7216b559ec2e89f4456
Nansamba Ssensalo <nssensalo@gmail.com>
2024-01-05 13:15:29 -0800

    artist mapping

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -30,11 +30,8 @@ export default function Artists() {
   return (
     <>
       {artists.map((artist) => (
-        <div style={{ marginBottom: 700 }} key={artist.id}>
-          <h1
-            className="posts-titles"
-            style={{ marginRight: 1000, fontSize: 40 }}
-          >
+        <div  key={artist.id}>
+          <h1 className="posts-titles">
             {artist.name}
           </h1>
           <hr
@@ -43,45 +40,44 @@ export default function Artists() {
               backgroundColor: '#D3D3D3',
               height: 1,
               margin: 20,
-              marginRight: 500,
+              
             }}
           />
 
-          <div>
+        <div>
             <img
-              style={{ float: 'left', marginLeft: 50, marginRight: 20 }}
+              style={{ float: 'left', marginRight: 20 }}
               src={`/${artist.image}.png`}
               height="300"
-              alt="Andrew Carriere Pic"
-            ></img>
+              alt="Andrew Carriere Pic">
+            </img>
             <p
               style={{
                 textAlign: 'justify',
-                marginRight: 500,
-                marginBottom: 40,
+                
               }}
             >
               {artist.bio}
             </p>
-          </div>
+        </div>
           <hr
             style={{
               border: '1px solid black',
               backgroundColor: '#D3D3D3',
               height: 1,
-              margin: 20,
-              marginRight: 500,
+              margin: 20
+              
             }}
           />
           <h2
             className="posts-titles"
-            style={{ textAlign: 'left', marginRight: 500 }}
+            
           >
             {artist.contentTitle}
           </h2>
           <img
-            style={{ marginRight: 500 }}
-            src={artist.content}
+            
+            src={`/${artist.content}.png`}
             width="700"
             height="400"
             alt="streamingSongs"
@@ -91,8 +87,8 @@ export default function Artists() {
               border: '1px solid black',
               backgroundColor: '#D3D3D3',
               height: 1,
-              margin: 20,
-              marginRight: 500,
+              margin: 20
+              
             }}
           />
         </div>

1a5c6a3b0d97d6385432dbfb92fa061651365aef
CElizOwens <owens.christina@gmail.com>
2024-02-22 17:45:21 -0800

    Add PORT env variable to vite config file, reignore .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const PORT: number = parseInt(process.env.PORT!) || 3000;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': `http://localhost:${PORT}`,
     },
   },
 });

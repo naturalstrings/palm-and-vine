142add3749f29c647c3d4d2c8e059a52bc6651c0
Nansamba Ssensalo <nssensalo@gmail.com>
2024-04-30 14:58:24 -0700

    commenting out artist path and files

--- src/components/Filters/ArtistNavbar.tsx
+++ src/components/Filters/ArtistNavbar.tsx
@@ -1,26 +1,26 @@
-import { artistData } from "./artistData";
-import MenuItems from './MenuItems';
-import './Filters.css';
+// import { artistData } from "./artistData";
+// import MenuItems from './MenuItems';
+// import './Filters.css';
 
-const ArtistNavbar = () => {
-    const depthLevel = 0;
-    return(
-                <nav className="desktop-nav">
-                    <ul className="menus">
-                        {artistData.map((menu,index) => {
-                            return (
-                            // <li  className="menu-items" key={index}  >
-                            //     <a href={menu.url}>{menu.title}</a>
-                            // </li>
-                            // <MenuItems  items={menu} key={index} depthLevel={depthLevel}/>
+// const ArtistNavbar = () => {
+//     const depthLevel = 0;
+//     return(
+//                 <nav className="desktop-nav">
+//                     <ul className="menus">
+//                         {artistData.map((menu,index) => {
+//                             return (
+//                             // <li  className="menu-items" key={index}  >
+//                             //     <a href={menu.url}>{menu.title}</a>
+//                             // </li>
+//                             // <MenuItems  items={menu} key={index} depthLevel={depthLevel}/>
                             
-                            )}
-                        )}
+//                             )}
+//                         )}
 
                     
-                    </ul>
-                </nav>
-            );
-};
+//                     </ul>
+//                 </nav>
+//             );
+// };
 
-export default ArtistNavbar;
+// export default ArtistNavbar;

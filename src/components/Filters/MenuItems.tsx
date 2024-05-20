90c675d8b4ad2fa6ff8cff2d46299509c0150e0a
Christina Walton <owens.christina@gmail.com>
2024-05-19 18:00:20 -0700

    Revert "Revert "preparing for render.com after commenting out all artist files""

--- src/components/Filters/MenuItems.tsx
+++ src/components/Filters/MenuItems.tsx
@@ -47,65 +47,65 @@
 // };
 // export default MenuItems;
 
-import { Link } from 'react-router-dom';
-import Dropdown from './Dropdown';
-import { useState, useEffect, useRef } from 'react';
+// import { Link } from 'react-router-dom';
+// import Dropdown from './Dropdown';
+// import { useState, useEffect, useRef } from 'react';
 
-// Interface for a single menu item
-interface MenuItem {
-  title: string;
-  url?: string;
-  submenu?: MenuItem[];
-}
+// // Interface for a single menu item
+// interface MenuItem {
+//   title: string;
+//   url?: string;
+//   submenu?: MenuItem[];
+// }
 
-// Interface for the component's props
-interface Props {
-  items: MenuItem[];
-  depthLevel: number;
-}
+// // Interface for the component's props
+// interface Props {
+//   items: MenuItem[];
+//   depthLevel: number;
+// }
 
-const MenuItems: React.FC<Props> = ({ items, depthLevel }) => {
-  const [dropdown, setDropdown] = useState(false);
+// const MenuItems: React.FC<Props> = ({ items, depthLevel }) => {
+//   const [dropdown, setDropdown] = useState(false);
 
-  const onMouseEnter = () => setDropdown(true);
-  const onMouseLeave = () => setDropdown(false);
+//   const onMouseEnter = () => setDropdown(true);
+//   const onMouseLeave = () => setDropdown(false);
 
-  const ref = useRef<HTMLLIElement>(null);
+//   const ref = useRef<HTMLLIElement>(null);
 
-  useEffect(() => {
-    const handler = (event: MouseEvent | TouchEvent) => {
-      if (dropdown && ref.current && !ref.current.contains(event.target)) {
-        setDropdown(false);
-      }
-    };
+//   useEffect(() => {
+//     const handler = (event: MouseEvent | TouchEvent) => {
+//       if (dropdown && ref.current && !ref.current.contains(event.target)) {
+//         setDropdown(false);
+//       }
+//     };
 
-    document.addEventListener('mousedown', handler);
-    document.addEventListener('touchstart', handler);
+//     document.addEventListener('mousedown', handler);
+//     document.addEventListener('touchstart', handler);
 
-    return () => {
-      document.removeEventListener('mousedown', handler);
-      document.removeEventListener('touchstart', handler);
-    };
-  }, [dropdown]);
+//     return () => {
+//       document.removeEventListener('mousedown', handler);
+//       document.removeEventListener('touchstart', handler);
+//     };
+//   }, [dropdown]);
 
-  return (
-    <ul className="menu-items">
-      {items.map((item) => (
-        <li key={item.title} ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-          {item.submenu ? (
-            <>
-              {item.title} {' '}
-              {depthLevel > 0 ? <i className="fa-solid fa-sm fa-angle-right"></i> : <i className="fa-solid fa-sm fa-angle-down"/>}
-              <Dropdown submenus={item.submenu} dropdown={dropdown} depthLevel={depthLevel} />
-            </>
-          ) : (
+//   return (
+//     <ul className="menu-items">
+//       {items.map((item) => (
+//         <li key={item.title} ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+//           {item.submenu ? (
+//             <>
+//               {item.title} {' '}
+//               {depthLevel > 0 ? <i className="fa-solid fa-sm fa-angle-right"></i> : <i className="fa-solid fa-sm fa-angle-down"/>}
+//               <Dropdown submenus={item.submenu} dropdown={dropdown} depthLevel={depthLevel} />
+//             </>
+//           ) : (
            
-            <Link href={items.url}>{items.title}</Link>
-          )}
-        </li>
-      ))}
-    </ul>
-  );
-};
+//             <Link href={items.url}>{items.title}</Link>
+//           )}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
 
-export default MenuItems;
+// export default MenuItems;

dd3007dd8a5ec0a50d73d5290462e324827812b7
CElizOwens <owens.christina@gmail.com>
2024-07-24 20:30:34 -0700

    Prettier formatting in EngineerFromDB.tsx

--- src/components/EngineerFromDB.tsx
+++ src/components/EngineerFromDB.tsx
@@ -1,62 +1,61 @@
-import { useState, useEffect} from 'react';
-import engineerpic1 from "/engineerpic1.png";
-import engineerpic2 from "/engineerpic2.png";
-import engineerpic3 from "/engineerpic3.png";
+import { useState, useEffect } from 'react';
+import engineerpic1 from '/engineerpic1.png';
+import engineerpic2 from '/engineerpic2.png';
+import engineerpic3 from '/engineerpic3.png';
 
 export default function EngineerFromDB() {
+  interface Engineer {
+    engineer_id: number;
+    name: string;
+    engineer_bio: string;
+  }
+  const [engineerList, setEngineer] = useState<Engineer[]>([]);
+  const engineerPics = [engineerpic1, engineerpic2, engineerpic3];
 
-    interface Engineer {
-    engineer_id: number,
-    name: string,
-    engineer_bio:string,
-}
-let ignore = false
-const [engineerList, setEngineer] = useState<Engineer[]>([]);
-const engineerPics = [engineerpic1,engineerpic2,engineerpic3];
+  useEffect(() => {
+    let ignore = false;
+    const getEngineer = async (): Promise<Engineer[] | undefined> => {
+      try {
+        const response = await fetch('api/engineer_bios');
+        console.log('*****Fetched response:', response);
+        const data: Engineer[] = (await response.json()) as Engineer[];
+        console.log('Engineer[]:', data);
+        return data;
+      } catch (err) {
+        console.log('Unable to fetch data:', err);
+      }
+    };
+    getEngineer()
+      .then((data) => {
+        if (!ignore && data) {
+          setEngineer(data);
+          console.log('getEngineer() completed.');
+        }
+      })
+      .catch((err) => {
+        console.log('getEngineer() did not complete:', err);
+      });
+    //cleanup
+    return () => {
+      ignore = true;
+    };
+  }, []); //useEffect end
 
-useEffect ( () => {
- 
-const getEngineer = async ():Promise<Engineer[] | undefined> => {
- 
-    try{
-    const response = await fetch( 'api/engineer_bios');
-    console.log('*****Fetched response:', response);
-    const data: Engineer[] = (await response.json()) as Engineer[];
-    console.log('Engineer[]:', data)
-    return data
-} catch (err){
-    console.log('Unable to fetch data:', err)
-} 
-
-};
-getEngineer()
-.then( (data) => {
-    if (!ignore && data) {
-        setEngineer(data);
-        console.log('getEngineer() completed.')
-    }
-}) .catch ((err) => {
-    console.log('getEngineer() did not complete:', err)
-})
-//cleanup
-return () => {
-    ignore = true;
-};
-
-
-},[]); //useEffect end
-
-return (
+  return (
     <div>
-            {engineerList.map((item,index) => (
-            <div key={ item.engineer_id } className='engineer-item'>
-               
-                    <h2>{item.name}<hr/></h2>
-                    <img src={engineerPics[index % engineerPics.length]} alt={item.name + 'Engineer Pic'}></img>
-                    <p>{item.engineer_bio}</p>
-               
-            </div>
-            ))}
+      {engineerList.map((item, index) => (
+        <div key={item.engineer_id} className="engineer-item">
+          <h2>
+            {item.name}
+            <hr />
+          </h2>
+          <img
+            src={engineerPics[index % engineerPics.length]}
+            alt={item.name + 'Engineer Pic'}
+          ></img>
+          <p>{item.engineer_bio}</p>
+        </div>
+      ))}
     </div>
- );
+  );
 }
